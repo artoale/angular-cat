806420e7444e9e3310cfc72550dbe78106adcf45
Alessandro Artoni <artoale@gmail.com>
2015-07-21 14:56:36 +0100

    Add paClass Controller APIs #7

--- src/js/animations/class.directive.js
+++ src/js/animations/class.directive.js
@@ -4,50 +4,96 @@ const mod = angular.module('pa.animations.class', []),
 mod.directive(directiveName, ($animate) => {
     return {
         restrict: 'A',
-        require: [],
+        require: [directiveName, '^?paRouter'],
+        controller($q, $scope, $attrs, $element) {
+            const className = $attrs[directiveName] || directiveName,
+                classNameStart = className + '--start',
+                statusScopeVar = $attrs.paStatus;
+
+            let status = '',
+                deferred,
+
+
+                resolve = (...args) => {
+                    return deferred && deferred.resolve(args);
+                },
+
+                setStatus = newStatus => {
+                    if (statusScopeVar) {
+                        $scope[statusScopeVar] = newStatus;
+                    }
+                    status = newStatus;
+                },
+
+                setUp = () => {
+                    setStatus('READY');
+                    $element.addClass(classNameStart);
+                },
+
+                clear = () => {
+                    let clearDeferred = $q.defer();
+                    clearDeferred.resolve();
+
+                    if (status === 'RUNNING') {
+                        //TODO we might want to reject this to handle this
+                        //usecase, needs more cowbell
+                        resolve();
+                    }
+
+                    setUp();
+
+                    return clearDeferred.promise;
+                },
+
+                runAnimation = () => {
+                    setStatus('RUNNING');
+
+                    $animate.removeClass(
+                        $element,
+                        classNameStart
+                    ).then(() => {
+                        setStatus('FINISHED');
+                        resolve();
+                    });
+
+                },
+                play = () => {
+                    if (status === 'READY') {
+                        deferred = $q.defer();
+                        runAnimation();
+                    }
+                    return deferred.promise;
+                };
+
+            //APIs used by linking function
+            this.setUp = setUp;
+            this.runAnimation = runAnimation;
+
+            //Public APIs
+            this.play = play;
+            this.resolve = resolve;
+            this.clear = clear;
+
+        },
         link(scope, element, attrs, controllers) {
             const selfController = controllers[0],
                 routerController = controllers[1],
-                animationName = attrs.paAnimationName || directiveName,
-                className = attrs[directiveName] || directiveName,
-                classNameStart = className + '--start',
-                status = attrs.paStatus,
-                undo = attrs.paUndo,
-
-                setStatus = newStatus => {
-                    if (status) {
-                        scope[status] = newStatus;
-                    }
-                },
-                setUp = () => {
-                    setStatus('READY');
-                    element.addClass(classNameStart);
-                };
+                animationName = attrs.paAnimationName || directiveName;
 
             if (routerController) {
                 routerController.register(animationName, selfController);
             }
 
-            setUp();
-
-            scope.$watch(attrs.paActive, (newVal) => {
-                if (newVal) {
-                    setStatus('RUNNING');
-
-                    $animate.removeClass(
-                        element,
-                        classNameStart
-                    ).then(() => {
-                        setStatus('FINISHED');
-                        // Used to be necessary in angular 1.3. Tests fail
-                        // when called, need to check on demo version.
-                        // scope.$apply();
-                    });
-                } else if (undo) {
-                    setUp();
-                }
-
-            });
+            selfController.setUp();
+            if (attrs.paActive) {
+                scope.$watch(attrs.paActive, (newVal) => {
+                    if (newVal) {
+                        selfController.runAnimation();
+                    } else if (attrs.paUndo) {
+                        selfController.setUp();
+                    }
+                });
+            }
 
         }
     };
