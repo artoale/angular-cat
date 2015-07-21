fca9b35c2d232bf4af454f2e94b1c345d36465f9
pchillari <pchillari@google.com>
2015-07-21 15:39:37 +0100

    Add new modules to the paAnimation one #5

--- src/js/paAnimations.js
+++ src/js/paAnimations.js
@@ -1,10 +1,12 @@
+import scrollSpy from './scroll-spy/scrollSpy.module'
 import animations from './animations/animations.module';
 import delay from './utils/delay.service';
 
 const module = angular.module('paAnimations', [
     'ngAnimate',
     animations.name,
-    delay.name
+    delay.name,
+    scrollSpy.name
 ]);
 
 export default module;
