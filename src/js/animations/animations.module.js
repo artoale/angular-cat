a368b174593cf9c6e257b8403c8d7c5388739b63
pchillari <pchillari@google.com>
2015-07-28 02:43:22 +0100

    Add video directive to animations module #21

--- src/js/animations/animations.module.js
+++ src/js/animations/animations.module.js
@@ -1,10 +1,12 @@
 import classDirective from './class.directive';
+import videoDirective from './video.directive';
 import delayDirective from './delay.directive';
 import routerDirective from './router.directive';
 
 const module = angular.module('pa.animations', [
     'ngAnimate',
     classDirective.name,
+    videoDirective.name,
     delayDirective.name,
     routerDirective.name
 ]);
