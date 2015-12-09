856d47e67363af3b4015b6a3a9128fe7634c4ee6
pchillari <pchillari@google.com>
2015-12-09 19:33:46 +0000

    Refactor delay directive services #41

--- test/animations/delay.directive.spec.js
+++ test/animations/delay.directive.spec.js
@@ -1,6 +1,10 @@
-describe('cat-delay directive', () => {
+describe.only('cat-delay directive', () => {
     let element,
         scope = {},
+        catAnimationLink,
+        catBaseAnimation,
+        catDelayS,
+        sandbox,
         template = `
             <div>
                 <div
@@ -12,22 +16,45 @@ describe('cat-delay directive', () => {
             </div>
             `,
         compile,
-        animate,
         timelineController = {
             register() {
                 return;
             }
         },
-        timeout;
-    beforeEach(angular.mock.module('cat.utils.delay'));
-    beforeEach(angular.mock.module('cat.animations.base-animation'));
-    beforeEach(angular.mock.module('cat.animations.animationLink'));
+        $timeout;
+
+
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create();
+    });
+
+
+    beforeEach(angular.mock.module('cat.animations.base-animation', function($provide) {
+        $provide.decorator('catBaseAnimation', function($delegate) {
+                return sandbox.spy($delegate);
+        });
+    }));
+
+    beforeEach(angular.mock.module('cat.animations.animationLink', function($provide) {
+        $provide.decorator('catAnimationLink', function($delegate) {
+                return sandbox.spy($delegate);
+        });
+    }));
+
+    beforeEach(angular.mock.module('cat.utils.delay', function($provide) {
+        $provide.decorator('catDelayS', function($delegate) {
+                return sandbox.spy($delegate);
+        });
+    }));
+
     beforeEach(angular.mock.module('cat.animations.delay'));
     beforeEach(angular.mock.module('ngAnimateMock'));
 
-    beforeEach(angular.mock.inject(($compile, $rootScope, $timeout, $animate) => {
+    beforeEach(angular.mock.inject(($compile, $rootScope, _$timeout_, _catAnimationLink_, _catBaseAnimation_, _catDelayS_) => {
         scope = $rootScope.$new();
-        animate = $animate;
+        catAnimationLink =  _catAnimationLink_;
+        catBaseAnimation =  _catBaseAnimation_;
+        catDelayS = _catDelayS_;
         scope.visible = false;
         compile = () => {
             let parentElement = angular.element(template);
@@ -37,9 +64,12 @@ describe('cat-delay directive', () => {
             scope.$apply();
         };
 
-        timeout =  $timeout;
+        $timeout = _$timeout_;
     }));
 
+    afterEach(() => {
+        sandbox.restore();
+    });
 
     it('should set the status to READY if variable is binded', () => {
         compile();
@@ -53,53 +83,11 @@ describe('cat-delay directive', () => {
         timelineController.register.should.have.been.calledWith('animation-name', element.controller('catDelay'));
     });
 
-    describe('active watch', () => {
-        it('should update status correctly', () => {
+    describe('#Linking Function', () => {
+        it('Should call catAnimationLink function', () => {
             compile();
-            scope.status.should.equal('READY');
-
-            scope.visible = true;
-            scope.$apply();
-
-            scope.status.should.equal('RUNNING');
-            timeout.flush(300);
-            scope.status.should.equal('FINISHED');
+            catAnimationLink.should.have.been.calledOnce;
         });
-
-        it('should reset status if undo is set', () => {
-            let oldTemplate = template;
-            template = `
-                <div>
-                    <div
-                        cat-delay="300"
-                        cat-active="visible"
-                        cat-status="status"
-                        cat-undo=1
-                    ></div>
-                </div>
-                `;
-            compile();
-
-            scope.visible = true;
-            scope.$apply();
-
-            scope.status.should.equal('RUNNING');
-            timeout.flush(100);
-
-            scope.status.should.equal('RUNNING');
-
-            scope.visible = false;
-            scope.$apply();
-
-            scope.status.should.equal('READY');
-
-            timeout.flush(200);
-
-            scope.status.should.equal('READY');
-            template = oldTemplate;
-        });
-
-
     });
 
     describe('controller', () => {
@@ -108,6 +96,26 @@ describe('cat-delay directive', () => {
         beforeEach(() => {
             compile();
             controller = element.controller('catDelay');
+        });
+
+        it('Should use catBaseAnimation', () => {
+            catBaseAnimation.should.have.been.calledOnce;
+        });
+
+        it('Should pass scope catBaseAnimation', () => {
+            var config;
+            config = catBaseAnimation.args[0][0];
+            config.$scope.should.equal(scope);
+        });
+
+        it('Should pass catBaseAnimation a config with defined onPlay, onSetUp, onClear, disable and attrs', () => {
+            var config;
+            config = catBaseAnimation.args[0][0];
+            config.$attrs.should.be.defined;
+            config.onSeek.should.be.function;
+            config.onPlay.should.be.function;
+            config.onSetUp.should.be.function;
+            config.disable.should.be.function;
         });
 
         describe('#play', () => {
@@ -119,11 +127,18 @@ describe('cat-delay directive', () => {
                 controller.play().then.should.be.defined;
             });
 
+            it('should call catDelayS with the specified delay', () => {
+                controller.play();
+                scope.$apply();
+                catDelayS.should.have.been.calledWith(300);
+            });
+
             it('should resolve the animation promise after the specified delay', () => {
-                let spy = sinon.spy();
+                let spy = sandbox.spy();
                 controller.play().then(spy);
+                scope.$apply();
                 spy.should.not.have.been.called;
-                timeout.flush(300);
+                $timeout.flush(300);
                 spy.should.have.been.calledOnce;
             });
 
@@ -134,17 +149,9 @@ describe('cat-delay directive', () => {
                 scope.$apply();
 
                 scope.status.should.equal('RUNNING');
-                timeout.flush(300);
+                $timeout.flush(300);
                 scope.status.should.equal('FINISHED');
             });
-
-        });
-
-        describe('#clear', () => {
-            it('should be defined', () => {
-                controller.clear.should.be.a('function');
-            });
-
         });
     });
 });
