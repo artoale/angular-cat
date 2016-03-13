875af37b35d6f1f30624a5f6c202d7458b80779d
Paolo <killer.paolo@gmail.com>
2016-03-13 14:25:26 +0100

    Remove spy and use angular-spy in the example

--- example/main.js
+++ example/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('example', ['cat']).run(function() {
+var app = angular.module('example', ['cat', 'ngSpy']).run(function() {
     console.log('I am running');
 });
 
--- src/js/cat.js
+++ src/js/cat.js
@@ -1,12 +1,10 @@
-import scrollSpy from './scroll-spy/scroll-spy.module';
 import animations from './animations/animations.module';
 import delay from './utils/delay.service';
 
 const module = angular.module('cat', [
     'ngAnimate',
     animations.name,
-    delay.name,
-    scrollSpy.name
+    delay.name
 ]);
 
 export default module;
